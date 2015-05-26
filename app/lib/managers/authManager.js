ff501ecabaff87a4c9699fd217d50bd972711984
jordanharris <jordanburrows@gmail.com>
2015-05-25 18:45:50 -0700

    updating menu tss

--- app/lib/managers/authManager.js
+++ app/lib/managers/authManager.js
@@ -45,6 +45,50 @@ exports.login = function (username, password, cb){
 	}); 
 };
 
+exports.userSignUp = function(firstName, lastName, username, password, cb) {
+	// Todo: validation
+	
+};	
+/*	// Prepare request
+	var loginRequest = {
+		"identifier": username,
+		"password": password
+	};
+	
+	httpClient.execute("/user", loginRequest, function(err, userSignUpResults) {
+		if(!err && loginResult) {
+			// Create a singleton
+			var authModel = Alloy.Models.instance('auth');
+			var userModel = Alloy.createModel('user');
+			
+			if(loginResult.user) {
+				// Set user properties
+				userModel.set({username: loginResult.user.username});
+				userModel.set({password: password });
+				userModel.set({firstName: loginResult.user.firstName});
+				userModel.set({lastName: loginResult.user.lastName});
+				userModel.set({email: loginResult.user.email});
+				userModel.set({id: loginResult.user.id});			
+			}
+			
+			authModel.set({ token: loginResult.token });
+			authModel.set({ username: username });
+			authModel.set({ password: password });
+			authModel.set({ user : JSON.stringify(userModel)});
+			authModel.set({ isAuth: true });
+			authModel.save();
+			
+			Ti.App.Properties.setString('token', loginResult.token);
+			Ti.App.Properties.setString('isAuth', true);
+			
+			if(cb) cb(null, authModel);
+			
+		} else {
+			if(cb) cb(err, null);
+		}
+	}); 
+};*/
+
 exports.isLoggedIn = function() {
 	return Ti.App.Properties.getString('isAuth') ? Ti.App.Properties.getString('isAuth') : false;
 };
