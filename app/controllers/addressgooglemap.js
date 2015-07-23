5a6e44bfdc9b289c73a7ea22d16df3b150e5b81b
jordanharris <jordanburrows@gmail.com>
2015-07-23 13:59:55 -0700

    fixed webview map button loop and edituserprofile page tss to work on all iphones

--- app/controllers/addressgooglemap.js
+++ app/controllers/addressgooglemap.js
@@ -1,27 +1,24 @@
 var args = arguments[0] || {};
 var address = [];
 
-Ti.App.addEventListener('app:cancelAddressWebView', function(e) {
+function cancelAddressWebView(){
 	Alloy.Globals.closePage('addressgooglemap');
 	address = [];
 	return;
-});
-
-Ti.App.addEventListener('app:verifyAddressWebView', function(e) {
+}
+function verifyAddressWebView(){
 	//verify that an address has been chosen via google places.
 	if (!address || !address.length) {
-		console.log("WE IN HERE");
 		alert("You must fill out an Address before you can verify!");
 	} else {
 		Alloy.Globals.openPage('verifyaddress', address);
 		console.log("verify adddress", address);
 		address = [];
-	}
+	}	
 	return;
-});
+}
 
 Ti.App.addEventListener('app:getAddressWebView', function(e) {
 	address.push(e.address);
-	console.log("getting adddress",address);
 	return;
 });
--- app/controllers/verifyaddress.js
+++ app/controllers/verifyaddress.js
@@ -1,6 +1,28 @@
 var args = arguments[0] || {};
+var address =  args[0];
 
-console.log('$%$%$%$$%$%$', args);
+for(i = 0; i < $.addressValidationView.children.length; i++) {
+	var row = $.addressValidationView.children[i];
+	if (address[row.children[0].id]) {
+		if (row.children[0].id === "route") {
+			row.children[0].value = address["street_number"] +" "+ address[row.children[0].id];
+		} else {
+			row.children[0].value = address[row.children[0].id];
+		}
+	}
+}
 
+
+$.verifyAddressCancelButton.addEventListener('click', function(){
+	Alloy.Globals.closePage('addressgooglemap');
+	Alloy.Globals.closePage('verifyaddress');
+});
+/*function closeValidateView(){
+	Alloy.Globals.closePage('verifyaddress');
+}*/
+
+function validateAddressView(){
+	//console.log("true");
+}
 
 //save to model..grab user object, update/ add address to user model. couchdb
