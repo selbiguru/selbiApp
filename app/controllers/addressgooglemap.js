912b318d233d656b306087583d97787e4ed90150
jordanharris <jordanburrows@gmail.com>
2015-07-24 12:15:12 -0700

    added additional logic to address validation and optimized verifyAddress with all iphones

--- app/controllers/addressgooglemap.js
+++ app/controllers/addressgooglemap.js
@@ -11,6 +11,11 @@ function verifyAddressWebView(){
 	if (!address || !address.length) {
 		alert("You must fill out an Address before you can verify!");
 	} else {
+		if (address[0].postal_code === "short_name") {
+			alert("This address does not have an associated Zip Code! Please enter a valid address.");
+			address = [];
+			return;
+		}
 		Alloy.Globals.openPage('verifyaddress', address);
 		console.log("verify adddress", address);
 		address = [];
--- app/controllers/verifyaddress.js
+++ app/controllers/verifyaddress.js
@@ -1,10 +1,9 @@
 var args = arguments[0] || {};
 var address =  args[0];
-
 for(i = 0; i < $.addressValidationView.children.length; i++) {
 	var row = $.addressValidationView.children[i];
 	if (address[row.children[0].id]) {
-		if (row.children[0].id === "route") {
+		if (row.children[0].id === "route" && address["street_number"] !== "short_name") {
 			row.children[0].value = address["street_number"] +" "+ address[row.children[0].id];
 		} else {
 			row.children[0].value = address[row.children[0].id];
@@ -17,9 +16,6 @@ $.verifyAddressCancelButton.addEventListener('click', function(){
 	Alloy.Globals.closePage('addressgooglemap');
 	Alloy.Globals.closePage('verifyaddress');
 });
-/*function closeValidateView(){
-	Alloy.Globals.closePage('verifyaddress');
-}*/
 
 function validateAddressView(){
 	//console.log("true");
