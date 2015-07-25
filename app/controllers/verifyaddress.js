3a7739d857cd52ea4f057d9313327f1520dc3ff9
jordanharris <jordanburrows@gmail.com>
2015-07-24 20:55:15 -0700

    updated validation utility for client side interaction and updating validation alert

--- app/controllers/verifyaddress.js
+++ app/controllers/verifyaddress.js
@@ -1,5 +1,8 @@
 var args = arguments[0] || {};
+var utils = require('utilities/validate');
 var address =  args[0];
+
+// Populates the address fields on page load
 for(i = 0; i < $.addressValidationView.children.length; i++) {
 	var row = $.addressValidationView.children[i];
 	if (address[row.children[0].id]) {
@@ -11,14 +14,32 @@ for(i = 0; i < $.addressValidationView.children.length; i++) {
 	}
 }
 
-
+//Closes addressgooglemap and verifyaddress Views on 'cancel' click
 $.verifyAddressCancelButton.addEventListener('click', function(){
 	Alloy.Globals.closePage('addressgooglemap');
 	Alloy.Globals.closePage('verifyaddress');
 });
 
+//
 function validateAddressView(){
-	//console.log("true");
+	var value = {
+		value: $.apartmenNumber.value,
+	};
+	var options = {
+		trim: true,
+		regexp: /^([A-Za-z0-9\-\_]*)$/,
+		label: "Apt/Street #",
+		required: false
+	};
+	var car = utils.validate(value, options);
+	if (car.message) {
+		alert(car.message);
+		//$.addClass($.userAptNumber, "error");
+		return;	
+	}
 }
 
+/*$.userAptNumber.addEventListener('click', function(){
+	$.removeClass($.userAptNumber, "error");
+});*/
 //save to model..grab user object, update/ add address to user model. couchdb
