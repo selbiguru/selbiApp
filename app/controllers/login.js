d755bd171751144f771efbab127f050d8f5b29f7
jordanharris <jordanburrows@gmail.com>
2016-02-12 00:06:17 -0800

    On login.js making sure the keyboard acts in accordance with expected behavior

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -27,6 +27,9 @@ function loginUser(){
 			helpers.alertUser('Login','Please check your Username and Password and try again!');
 			return;
 		} else {
+			$.loginView.removeEventListener('click', blurTextField);
+			$.username.removeEventListener('return', keyboardNext);
+			$.password.removeEventListener('return', keyboardLogIn);
 			indicatorWindow.closeIndicator();
 			buttonOn();
 			var homeController = Alloy.createController('masterlayout').getView();
@@ -54,3 +57,54 @@ function buttonOn() {
 	$.forgotPassword.touchEnabled = true;
 	$.closeWindow.touchEnabled = true;
 }
+
+
+
+/**
+ * @private blurTextField 
+ * Blurs username and password text field in accordance with expected UI
+ */
+function blurTextField(e) {
+	if(e.source.id === 'username') {
+		$.username.focus();
+		$.password.blur();
+	} else if(e.source.id === 'password') {
+		$.password.focus();
+		$.username.blur();
+	} else {
+		$.password.blur();
+		$.username.blur();
+	}
+};
+
+
+/**
+ * @method keyboardNext
+ * Clicking the keyboard 'NEXT' button takes the user from username to password
+ */
+function keyboardNext() {
+	$.username.blur();
+	$.password.focus();
+};
+
+
+/**
+ * @method keyboardLogIn
+ * Clicking the keyboard 'GO' button attempts to sign the user in
+ */
+function keyboardLogIn() {
+	$.password.blur();
+	loginUser();
+}
+
+
+
+/*-------------------------------------------------Event Listeners---------------------------------------------------*/
+
+
+
+$.loginView.addEventListener('click', blurTextField);
+
+$.username.addEventListener('return', keyboardNext);
+
+$.password.addEventListener('return', keyboardLogIn);
